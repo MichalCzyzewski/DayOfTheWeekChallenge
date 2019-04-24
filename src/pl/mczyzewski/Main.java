4495f5991be70ae9c1abcdc51c1e2116ce367999
Michal Czyzewski <czy.mich@gmail.com>
2019-04-24 14:20:04 +0200

    Initial commit

---
+++ src/pl/mczyzewski/Main.java
@@ -0,0 +1,16 @@
+package pl.mczyzewski;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int day = 0;
+
+        switch (day){
+            case 1 :
+                System.out.println("Monday");
+        }
+
+
+    }
+}
