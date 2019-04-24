012bf386fd3f664465895304b119b1537ea63a18
Michal Czyzewski <czy.mich@gmail.com>
2019-04-24 14:35:00 +0200

    add privet method

--- src/pl/mczyzewski/Main.java
+++ src/pl/mczyzewski/Main.java
@@ -4,22 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int day = 1;
+        printDayOfTheWeek(1);
 
+    }
+
+    private static void printDayOfTheWeek(int day)
+    {
         switch (day){
             case 0 :
                 System.out.println("Monday");break;
             case 1:
                 System.out.println("Tuesday");break;
-                case 2 :
+            case 2 :
                 System.out.println("Wednesday");break;
             case 3:
                 System.out.println("Thursday");break;
-                case 4 :
+            case 4 :
                 System.out.println("Friday");break;
             case 5:
                 System.out.println("Saturday");break;
-                case 6 :
+            case 6 :
                 System.out.println("Sunday");break;
             default:
                 System.out.println("Invalid Day");break;
@@ -51,7 +55,6 @@ public class Main {
         {
             System.out.println("if Invalid Day");
         }
-
 
 
     }
